cfff20e4b67fb02eb81a28ab46a2f569db298b30
André Verzoto <118853023+AVerz26@users.noreply.github.com>
2025-09-16 11:36:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ def buscar_leads(area, cidade, num_resultados, api_key, delay=2):
             "google_domain": "google.com.br",
             "hl": "pt",
             "gl": "br",
-            "start": start,     # sempre múltiplo de 20
             "api_key": api_key
         }
 
